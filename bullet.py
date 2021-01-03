43a9c13eebee0b7a7b157aa932050422f963dfab
samuelsallee <76776702+samuelsallee@users.noreply.github.com>
2021-01-03 20:00:40 +0700

    Update bullet.py

--- bullet.py
+++ bullet.py
@@ -1,24 +1,22 @@
 import Header
 from Header import*
 
-bulletList = list()
-
 
 class Bullet:
-    damage: int = 0
-    direction = 0
-    speed = 0
-    location = (0, 0)
-    image = pygame.image.load("Transparent bullet.png")
 
-    def __init__(self, dam, direct, spd, loc):
-        damage = dam
-        direction = direct
-        speed = spd
-        location = loc
+    image = pygame.transform.scale(pygame.image.load("Transparent bullet.png"),(30,20))
+
+    def __init__(self, dam, direct, spd, locx, locy, chx, chy):
+        self.damage = dam
+        self.direction = direct
+        self.speed = spd
+        self.locationx = locx
+        self.locationy = locy
+        self.changex = chx
+        self.changey = chy
 
     def set_damage(self, dam: int):
-        damage = dam
+        self.damage = dam
 
     def set_direction(self, direct: int):
         direction = direct
@@ -26,17 +24,25 @@ class Bullet:
     def set_speed(self, sp: int):
         speed = sp
 
-    def set_location(self, loc):
-        location = loc
+    def set_location(self, locx, locy):
+        self.locationx = locx
+        self.locationy = locy
 
     def get_damage(self,):
-        return damage
+        return self.damage
 
     def get_direction(self,):
-        return direction
+        return self.direction
 
     def get_speed(self,):
-        return speed
+        return self.speed
 
-    def get_location(self,):
-        return location
+    def get_locationx(self,):
+        return self.locationx
+
+
+bullet1 = Bullet(0, 0, 0, 0, 0, 0, 0)
+bullet2 = Bullet(0, 0, 0, 0, 0, 0, 0)
+bulletList = list((bullet1, bullet2))
+bulletList.pop()
+bulletList.pop()
