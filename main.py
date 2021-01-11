2a5dc8aa95213cd3a12e59820268466066781b42
samuelsallee <76776702+samuelsallee@users.noreply.github.com>
2021-01-11 08:52:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ SCREEN_WIDTH: int = 800
 SCREEN_HEIGHT: int = 600
 FPS: int = 60
 FramesPerSecond = pygame.time.Clock()
+enemyList = list()
 
 if pygame.init() == 0:
     print("PyGame could not initialize")
@@ -22,6 +23,11 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 playerImage = pygame.image.load("really tiny soldier.png")
 
+bullet_damage: int = 40
+bullet_speed: int = 10
+
+Goblin = enemy(0, 0, 64, 64, 2, 550, 100)
+
 
 def player(angle, x, y):
     rot_radian = math.atan2((angle[1] - y), (angle[0] - x))
@@ -32,7 +38,6 @@ def player(angle, x, y):
 
 def draw_bullet():
     for bullet_object in bullet.bulletList:
-        #if bullet.locy - radian < Goblin.hitbox[1]+Goblin.hitbox[2] and bullet.locy# hit detection
         if bullet_object.locationx > SCREEN_WIDTH or bullet_object.locationx < 0 or bullet_object.locationy > SCREEN_HEIGHT or bullet_object.locationy < 0:
             bullet.bulletList.remove(bullet_object)
         else:
@@ -41,17 +46,37 @@ def draw_bullet():
             bullet_object.set_location(bullet_object.locationx + bullet_object.changex, bullet_object.locationy + bullet_object.changey)
 
 
+def draw_enemy():
+    for enemy_object in enemyList:
+        if enemy_object.health < 1:
+            enemyList.remove(enemy_object)
+            Goblin = enemy(0, 0, 64, 64, 2, 550, 100)
+            enemyList.append(Goblin)
+        else:
+            enemy_object.draw(screen)
+
+
+def hit_logic():
+    for bullet_object in bullet.bulletList:
+        for enemy_object in enemyList:
+            if bullet_object.locationx > enemy_object.x and bullet_object.locationx < enemy_object.x + enemy_object.width:
+                if bullet_object.locationy > enemy_object.y and bullet_object.locationy < enemy_object.y + enemy_object.width:
+                    bullet_object.locationx = -6
+                    enemy_object.health -= bullet_object.damage
+
+
 
 player_x = 400
 player_y = 300
 xDelta = 0
 yDelta = 0
-Goblin = enemy(0, 0, 64, 64, 2, 550) #initalize enemy
 running = True
 
 while running:
     screen.fill((0, 0, 0))
-    Goblin.draw(screen) #draw the enmy on the screen
+    if len(enemyList) == 0:
+        enemyList.append(Goblin)
+
     mouse = pygame.mouse.get_pos()
 
     for event in pygame.event.get():
@@ -65,7 +90,7 @@ while running:
             direct = -1 * math.degrees(radian)
             buldeltay = math.sin(radian) * 10
             buldeltax = math.cos(radian) * 10
-            new_bullet = bullet.Bullet(40, direct, 10, player_x + (math.cos(radian+.45) * 21), player_y + (math.sin(radian + .45) * 21), buldeltax, buldeltay)
+            new_bullet = bullet.Bullet(bullet_damage, direct, bullet_speed, player_x + (math.cos(radian+.45) * 21), player_y + (math.sin(radian + .45) * 21), buldeltax, buldeltay)
             bullet.bulletList.append(new_bullet)
             Gunfire.play()
 
@@ -103,5 +128,7 @@ while running:
 
     player(mouse, player_x, player_y)
     draw_bullet()
+    draw_enemy()
+    hit_logic()
     pygame.display.update()
     FramesPerSecond.tick(FPS)
