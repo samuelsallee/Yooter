b015b109ee0f3474f22a19ab8e16fdcc0c322433
Florian Bihler <florianlaurenz.bihler@gmail.com>
2021-01-22 16:27:40 +0700

    Return to working version

--- menu.py
+++ menu.py
@@ -1,23 +1,37 @@
 import header
 
 class menu:
-    def loopMenu(fontSize: int):
-        menuRunning: bool = True
-        while menuRunning == True:
-            fontSize = 160 #del
-            Font = pygame.font.SysFont('comicsans', fontSize, True, True)
-            screen.blit(pauseFont.render("Pause", True, (0, 0, 0)), (SCREEN_WIDTH/2-fontSize, SCREEN_HEIGHT/2-fontSize/2))
-            for event in pygame.event.get():
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_ESCAPE:
-                        menuRunning = False
-            pygame.display.update()
-            FramesPerSecond.tick(FPS)
-            draw.draw_pause_menu(screen, enemyList, background_x, background_y, background, SCREEN_WIDTH, SCREEN_HEIGHT)
+    def init(self, fontProportions: int):
+        FontText = pygame.font.SysFont('comicsans', fontProportions, True, True)
+        FontHeadline = pygame.font.SysFont('comicsans', 2*fontProportions, True, True)
+        pygame.display.update()
+        FramesPerSecond.tick(FPS)
+        draw.draw_pause_menu(screen, enemyList, background_x, background_y, background, SCREEN_WIDTH, SCREEN_HEIGHT)
 
 class pauseMenu(menu):
-    None
+        def loopMenu(self, fontSize: int):
+            menuRunning: bool = True
+            while menuRunning == True:
+                init(160)
+                screen.blit(FontHeadline.render("Pause", True, (0, 0, 0)), (screen.get_width()/2-FontHeadline.size()[0], screen.get_height()/2-FontHeadline.size()[1]/2))
+                for event in pygame.event.get():
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_ESCAPE:
+                            menuRunning = False
+                    if event.type == VIDEORESIZE:
+                        player_one.position_x = screen.get_width() / 2
+                        player_one.position_y = screen.get_height() / 2
+
 
 class mainMenu(menu):
-    None
+        def loopMenu(self):
+            menuRunning: bool = True
+            while menuRunning == True:
+                init(160)
+                screen.blit(FontText.render("Pause", True, (0, 0, 0)), (screen.get_width()/2-FontText.size()[0], screen.get_height()/2-FontText.size()[1]/2))
+                for event in pygame.event.get():
+                    if event.type == VIDEORESIZE:
+                        player_one.position_x = screen.get_width() / 2
+                        player_one.position_y = screen.get_height() / 2
+                pygame.draw.rect(screen.get_width()/2, screen.get_height()/2, 200, 20)
 
