d745eb39fcd439a0d6ec808575b61fce4e8f6600
samuelsallee <76776702+samuelsallee@users.noreply.github.com>
2021-01-23 15:57:43 +0700

    Game over "yes" and "no" are clickable

--- main.py
+++ main.py
@@ -4,10 +4,7 @@ from enemy import enemy
 import draw
 import player
 
-#trying to use github with pycharm
-
 testing = 0
-running: bool = True
 
 start = float(round(time.time()))
 SCREEN_WIDTH: int = 800
@@ -22,7 +19,6 @@ if pygame.init() == 0:
 logo = pygame.image.load("8bitlink.png")
 pygame.display.set_icon(logo)
 pygame.display.set_caption("# Learn to Code")
-
 
 if testing == 0:
     pygame.mixer.music.load('bgmusic.wav')
@@ -52,10 +48,12 @@ def hit_logic(person1):
                     enemy_object.health -= bullet_object.damage
                     if enemy_object.health < 1:
                         enemyList.remove(enemy_object)
-                        global score #uses global varriable score inside the function
-                        score = score+5 #increases score by 5 for every kill
-        player_to_enemy_distance_tuple = (person1.player_center[0] - enemy_object.center[0], person1.player_center[1] - enemy_object.center[1])
-        p_to_e_distance = player_to_enemy_distance_tuple[0]*player_to_enemy_distance_tuple[0] + player_to_enemy_distance_tuple[1] * player_to_enemy_distance_tuple[1]
+                        global score  # uses global varriable score inside the function
+                        score = score + 5  # increases score by 5 for every kill
+        player_to_enemy_distance_tuple = (
+        person1.player_center[0] - enemy_object.center[0], person1.player_center[1] - enemy_object.center[1])
+        p_to_e_distance = player_to_enemy_distance_tuple[0] * player_to_enemy_distance_tuple[0] + \
+                          player_to_enemy_distance_tuple[1] * player_to_enemy_distance_tuple[1]
         p_to_e_distance = math.sqrt(p_to_e_distance)
         if testing == 0:
             if p_to_e_distance < 15:
@@ -63,7 +61,7 @@ def hit_logic(person1):
     return True
 
 
-font = pygame.font.SysFont('comicsans', 30, True, True) # Initializes Font
+font = pygame.font.SysFont('comicsans', 30, True, True)  # Initializes Font
 
 background_x: int = -500
 background_y: int = -500
@@ -72,22 +70,25 @@ yDelta: float = 0
 extra_enemies: int = 0
 pauseMenu: bool = False
 
-#background = pygame.image.load("backgrounddetailed1_flower.png")
+# background = pygame.image.load("backgrounddetailed1_flower.png")
 background = pygame.image.load("backgrounddetailed1.png")
 
-def setBackground():
-    randNum: int = int(random.random()*10)
-    if randNum < 5:
+
+def set_background():
+    rand_num: int = int(random.random() * 10)
+    if rand_num < 5:
         background = pygame.image.load("backgrounddetailed1_flower.png")
     else:
         background = pygame.image.load("backgrounddetailed1.png")
+
 
 def runPauseMenu():
     pauseMenuOff: bool = False
     fontSize: int = 200
     pauseFont = pygame.font.SysFont('comicsans', fontSize, True, True)
     while pauseMenuOff == False:
-        screen.blit(pauseFont.render("Pause", True, (0, 0, 0)), (screen.get_width()/2-fontSize, screen.get_height()/2-fontSize/2))
+        screen.blit(pauseFont.render("Pause", True, (0, 0, 0)),
+                    (screen.get_width() / 2 - fontSize, screen.get_height() / 2 - fontSize / 2))
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
@@ -97,123 +98,141 @@ def runPauseMenu():
                 player_one.position_y = screen.get_height() / 2
         pygame.display.update()
         FramesPerSecond.tick(FPS)
-        draw.draw_pause_menu(screen, enemyList, background_x, background_y, background, screen.get_width(), screen.get_height())
+        draw.draw_pause_menu(screen, enemyList, background_x, background_y, background, screen.get_width(),
+                             screen.get_height())
 
 
-while running:
+game_quit: bool = False
 
+while not game_quit:
+    running: bool = True
+    while running:
 
-    if len(enemyList) == 0:
-        i: int = 0
-        now = float(round(time.time()))
-        while i < extra_enemies:
-            around = float(random.random() * 10)
-            Goblin = enemy(math.cos(around) * 500 + screen.get_width() / 2, math.sin(around) * 500 + screen.get_height() / 2, 64,
-                           64, 2, 550,
-                           100 + (now - start) / 10)
-            enemyList.append(Goblin)
-            i += 1
-        extra_enemies += 1
+        if len(enemyList) == 0:
+            i: int = 0
+            now = float(round(time.time()))
+            while i < extra_enemies:
+                around = float(random.random() * 10)
+                Goblin = enemy(math.cos(around) * 500 + screen.get_width() / 2,
+                               math.sin(around) * 500 + screen.get_height() / 2, 64,
+                               64, 2, 550,
+                               100 + (now - start) / 10)
+                enemyList.append(Goblin)
+                i += 1
+            extra_enemies += 1
 
-    mouse = pygame.mouse.get_pos()
-    draw.draw(mouse, screen.get_width(), screen.get_height(), screen, enemyList, background, xDelta, yDelta, background_x, background_y, player_one)
+        mouse = pygame.mouse.get_pos()
+        draw.draw(mouse, screen.get_width(), screen.get_height(), screen, enemyList, background, xDelta, yDelta,
+                  background_x, background_y, player_one)
 
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            running = False
-            pygame.quit()
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                running = False
+                pygame.quit()
 
-        elif event.type == VIDEORESIZE:
-            player_one.position_x = screen.get_width() / 2
-            player_one.position_y = screen.get_height() / 2
+            elif event.type == VIDEORESIZE:
+                player_one.position_x = screen.get_width() / 2
+                player_one.position_y = screen.get_height() / 2
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            radian = math.atan2((mouse[1] - player_one.position_y), (mouse[0] - player_one.position_x))
-            direct = -1 * math.degrees(radian)
-            buldeltay = math.sin(radian) * 10
-            buldeltax = math.cos(radian) * 10
-            new_bullet = bullet.Bullet(bullet_damage, direct, bullet_speed, player_one.position_x + (math.cos(radian + .45) * 21),
-                                       player_one.position_y + (math.sin(radian + .45) * 21), buldeltax, buldeltay)
-            bullet.bulletList.append(new_bullet)
-            Gunfire.play()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if pygame.mouse.get_pressed(5)[0]:
+                    radian = math.atan2((mouse[1] - player_one.position_y), (mouse[0] - player_one.position_x))
+                    direct = -1 * math.degrees(radian)
+                    buldeltay = math.sin(radian) * 10
+                    buldeltax = math.cos(radian) * 10
+                    new_bullet = bullet.Bullet(bullet_damage,
+                                               direct,
+                                               bullet_speed,
+                                               player_one.position_x + (math.cos(radian + .45) * 21),
+                                               player_one.position_y + (math.sin(radian + .45) * 21),
+                                               buldeltax,
+                                               buldeltay)
+                    bullet.bulletList.append(new_bullet)
+                    Gunfire.play()
 
-        if event.type == pygame.KEYDOWN:
+            if event.type == pygame.KEYDOWN:
 
-            if event.key == pygame.K_a:
-                xDelta -= 5
-            if event.key == pygame.K_d:
-                xDelta += 5
-            if event.key == pygame.K_w:
-                yDelta -= 5
-            if event.key == pygame.K_s:
-                yDelta += 5
-            if event.key == pygame.K_ESCAPE:
-                pauseMenu = True
+                if event.key == pygame.K_a:
+                    xDelta -= 5
+                if event.key == pygame.K_d:
+                    xDelta += 5
+                if event.key == pygame.K_w:
+                    yDelta -= 5
+                if event.key == pygame.K_s:
+                    yDelta += 5
+                if event.key == pygame.K_ESCAPE:
+                    pauseMenu = True
 
-        if event.type == pygame.KEYUP:
+            if event.type == pygame.KEYUP:
 
-            if event.key == pygame.K_a:
-                xDelta += 5
-            if event.key == pygame.K_d:
-                xDelta -= 5
-            if event.key == pygame.K_w:
-                yDelta += 5
-            if event.key == pygame.K_s:
-                yDelta -= 5
-    if pauseMenu == True:
-        runPauseMenu()
-        pauseMenu = False
-    if player_one.position_x < 0:
-        player_one.position_x = 0
-    if player_one.position_x > screen.get_width() - playerImage.get_size()[0]:
-        player_one.position_x = screen.get_width() - playerImage.get_size()[0]
-    if player_one.position_y < 0:
-        player_one.position_y = 0
-    if player_one.position_y > screen.get_height() - playerImage.get_size()[1]:
-        player_one.position_y = screen.get_height() - playerImage.get_size()[1]
+                if event.key == pygame.K_a:
+                    xDelta += 5
+                if event.key == pygame.K_d:
+                    xDelta -= 5
+                if event.key == pygame.K_w:
+                    yDelta += 5
+                if event.key == pygame.K_s:
+                    yDelta -= 5
+        if pauseMenu:
+            runPauseMenu()
+            pauseMenu = False
+        if player_one.position_x < 0:
+            player_one.position_x = 0
+        if player_one.position_x > screen.get_width() - playerImage.get_size()[0]:
+            player_one.position_x = screen.get_width() - playerImage.get_size()[0]
+        if player_one.position_y < 0:
+            player_one.position_y = 0
+        if player_one.position_y > screen.get_height() - playerImage.get_size()[1]:
+            player_one.position_y = screen.get_height() - playerImage.get_size()[1]
 
-    background_x -= xDelta
-    background_y -= yDelta
-    if background_x >= 0:
-        background_x = -500
-    if background_y >= 0:
-        background_y = -500
-    running = hit_logic(player_one)
-    scoretxt = font.render("Score: " + str(score),True,(0,0,0))
-    screen.blit(scoretxt,(0,0))
-    pygame.display.update()
-    FramesPerSecond.tick(FPS)
+        background_x -= xDelta
+        background_y -= yDelta
+        if background_x >= 0:
+            background_x = -500
+        if background_y >= 0:
+            background_y = -500
+        running = hit_logic(player_one)
+        scoretxt = font.render("Score: " + str(score), True, (0, 0, 0))
+        screen.blit(scoretxt, (0, 0))
+        pygame.display.update()
+        FramesPerSecond.tick(FPS)
 
-###############################################################################
-#THIS IS WHERE THE GAME OVER LOOP STARTS
-###############################################################################
+    ###############################################################################
+    # THIS IS WHERE THE GAME OVER LOOP STARTS
+    ###############################################################################
 
-game_over: bool = True
-fontSize: int = 100
-fontSize2: int = 50
-fontSize3: int = 30
-gameOverFont = pygame.font.SysFont('comicsans', fontSize, True, True)
-gameOverFont2 = pygame.font.SysFont('comicsans', fontSize2, True, True)
-gameOverFont3 = pygame.font.SysFont('comicsans', fontSize3, True, True)
+    enemyList.clear()
+    bullet.bulletList.clear()
+    extra_enemies = 0
+    game_over: bool = True
+    fontSize: int = 100
+    fontSize2: int = 50
+    fontSize3: int = 30
+    gameOverFont = pygame.font.SysFont('comicsans', fontSize, True, True)
+    gameOverFont2 = pygame.font.SysFont('comicsans', fontSize2, True, True)
+    gameOverFont3 = pygame.font.SysFont('comicsans', fontSize3, True, True)
 
-while game_over:
+    yes_tuple = (263, 343, 55, 30)
+    no_tuple = (487, 343, 55, 30)
 
-    screen.fill([255, 0, 0])
-    mouse = pygame.mouse.get_pos()
+    while game_over:
 
-    pygame.draw.rect(screen, (125, 125, 125), (263, 343, 55, 30))
-    pygame.draw.rect(screen, (125, 125, 125), (487, 343, 55, 30))
-    screen.blit(gameOverFont.render("GAME OVER NERD", True, (0, 0, 0)), (50, 100))
-    screen.blit(gameOverFont2.render("YOU SUCK SHIT", True, (0, 0, 0)), (250, 220))
-    screen.blit(gameOverFont3.render("Would you like to try again?", True, (0, 0, 0)), (240, 300))
-    screen.blit(gameOverFont3.render("Yes", True, (0, 0, 0)), (270, 350))
-    screen.blit(gameOverFont3.render("No", True, (0, 0, 0)), (500, 350))
+        mouse = pygame.mouse.get_pos()
+        draw.draw_game_over_screen(screen, yes_tuple, no_tuple, gameOverFont, gameOverFont2, gameOverFont3)
+        pygame.display.update()
+        FramesPerSecond.tick(FPS)
 
-    pygame.display.update()
-    FramesPerSecond.tick(FPS)
-
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            game_over = False
-            pygame.quit()
-
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                game_over = False
+                pygame.quit()
+            elif event.type == MOUSEBUTTONDOWN:
+                if pygame.mouse.get_pressed(3)[0]:
+                    if yes_tuple[0] < mouse[0] < yes_tuple[0] + yes_tuple[2]:
+                        if yes_tuple[1] < mouse[1] < yes_tuple[1] + yes_tuple[3]:
+                            game_over = False
+                            print("a")
+                    elif no_tuple[0] < mouse[0] < no_tuple[0] + no_tuple[2]:
+                        if no_tuple[1] < mouse[1] < no_tuple[1] + no_tuple[3]:
+                            game_over = False
+                            game_quit = True
--- menu.py
+++ menu.py
@@ -1,23 +1,43 @@
-import header
+from header import *
+import draw
 
-class menu:
-    def loopMenu(fontSize: int):
-        menuRunning: bool = True
-        while menuRunning == True:
-            fontSize = 160 #del
-            Font = pygame.font.SysFont('comicsans', fontSize, True, True)
-            screen.blit(pauseFont.render("Pause", True, (0, 0, 0)), (SCREEN_WIDTH/2-fontSize, SCREEN_HEIGHT/2-fontSize/2))
-            for event in pygame.event.get():
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_ESCAPE:
-                        menuRunning = False
-            pygame.display.update()
-            FramesPerSecond.tick(FPS)
-            draw.draw_pause_menu(screen, enemyList, background_x, background_y, background, SCREEN_WIDTH, SCREEN_HEIGHT)
+class menu():
+    FontHeadline: pygame.font
+    FontText: pygame.font
+    def init(fontProportions: int, screen, enemyList, background_x, background_y, background):
+        FontText = pygame.font.SysFont('comicsans', fontProportions, True, True)
+        FontHeadline = pygame.font.SysFont('comicsans', 2*fontProportions, True, True)
+        pygame.display.update()
+        import main
+        main.FramesPerSecond.tick(main.FPS)
+        draw.draw_pause_menu(screen, enemyList, background_x, background_y, background, screen.get_width(), screen.get_height())
 
-class pauseMenu(menu):
-    None
+class pauseMenu(menu):    
+    def loopMenu(screen, enemyList, background_x, background_y, background):
+            menuRunning: bool = True
+            while menuRunning == True:
+                menu.init(160, screen, enemyList, background_x, background_y, background)
+                screen.blit(menu.FontHeadline.render("Pause", True, (0, 0, 0)), (screen.get_width()/2-menu.FontHeadline.size()[0], screen.get_height()/2-menu.FontHeadline.size()[1]/2))
+                for event in pygame.event.get():
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_ESCAPE:
+                            menuRunning = False
+                    if event.type == VIDEORESIZE:
+                        import main
+                        main.player_one.position_x = screen.get_width() / 2
+                        main.player_one.position_y = screen.get_height() / 2
+
 
 class mainMenu(menu):
-    None
+        def loopMenu():
+            menuRunning: bool = True
+            while menuRunning == True:
+                menu.init(160)
+                screen.blit(FontText.render("Pause", True, (0, 0, 0)), (screen.get_width()/2-FontText.size()[0], screen.get_height()/2-FontText.size()[1]/2))
+                for event in pygame.event.get():
+                    if event.type == VIDEORESIZE:
+                        import main
+                        main.player_one.position_x = screen.get_width() / 2
+                        main.player_one.position_y = screen.get_height() / 2
+                #pygame.draw.rect(screen.get_width()/2, screen.get_height()/2, 200, 20)
 
