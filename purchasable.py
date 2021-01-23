2bcdfd3b6c90466e09b8379f97136b7d0aed835a
samuelsallee <76776702+samuelsallee@users.noreply.github.com>
2021-01-23 23:53:06 +0700

    purchasable classes updates slightly

--- purchasable.py
+++ purchasable.py
@@ -12,3 +12,17 @@ class placeable(things_to_buy):
         self.y: int = mouse[1]
         self.overall_x = player.overall_position_x + mouse[0] - player.position_x
         self.overall_y = player.overall_position_y - mouse[1] - player.position_y
+
+
+class wooden_wall(placeable):
+    def __init__(self, image):
+        self.image = image
+
+
+class stone_wall(placeable):
+    def __init__(self, image):
+        self.image = image
+
+class turret(placeable):
+    def __init__(self, image):
+        self.image = image
